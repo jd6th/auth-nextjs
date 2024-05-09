ffb19b01dfddfe622a91e13fea466d0413c95e64
joydalud <joy.dalud@gmail.com>
2024-05-09 15:29:09 +0800

    send verification email

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,12 @@
+import { NewVerificationForm } from "@/components/auth/new-verification-form"
+
+const NewVerificationPage = () => {
+    return (
+        <div className="flex justify-center items-center py-10">
+            <NewVerificationForm />
+        </div>
+        
+    )
+}
+
+export default NewVerificationPage
